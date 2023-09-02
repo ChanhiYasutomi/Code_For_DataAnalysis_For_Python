eb95119dcfd0991a59c7b2a592f8606e2e018d41
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-02 17:05:26 +0900

    cross tabulation.py

--- cross tabulation.py
+++ cross tabulation.py
@@ -1,2 +1,5 @@
 # クロス集計
 pd.crosstab(dataframe['columns'], dataframe['columns']).reset_index()
+
+# pd.crosstab(dataframe['columns_1'], dataframe['columns_2'], dataframe['columns'_3]).reset_index()のようにカラムが3つある場合に、
+# dataframe['columns_1']かつdataframe['columns_2']の条件(行)でdataframe['columns'_3](列)のクロス集計になる。
