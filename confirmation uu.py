7f68872e5af84ff4219ab5baf236c70dace6a2fe
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-02 18:43:35 +0900

    confirmation uu.py

--- confirmation uu.py
+++ confirmation uu.py
@@ -1,5 +1,5 @@
 # technician
-technician_tmp = df_technician[['BC ID','technician　Name']].drop_duplicates()
+technician_tmp = df_technician[['BC ID','technician　Name']].drop_duplicates()　#ユニークにしたい
 merge_tmp_technician = pd.merge(video_log[columns], technician_tmp ,how = 'left', on = 'ID')
 
 a = merge_tmp_technician[['Id','technician Name']].drop_duplicates()
@@ -9,7 +9,7 @@ b['technician Name'] = b['technician Name'].fillna(0)
 pd.crosstab(b['technician Name'],b['frequency']).reset_index()
 
 # category
-category_tmp = df_video_tags[['Categories',"BC ID"]].drop_duplicates()
+category_tmp = df_video_tags[['Categories',"BC ID"]].drop_duplicates()　#ユニークにしたい
 merge_tmp_category = pd.merge(video_log[columns], category_tmp  ,how = 'left', on = 'ID')
 
 a = merge_tmp_category[['Id','Content Categories']].drop_duplicates()
@@ -19,7 +19,7 @@ b['Categories'] = b['Categories'].fillna(0)
 pd.crosstab(b['Categories'],b['frequency']).reset_index()
 
 # tech
-tech_tmp = df_video_tags[['tech name',"BC ID"]].drop_duplicates()
+tech_tmp = df_video_tags[['tech name',"BC ID"]].drop_duplicates()　#ユニークにしたい
 merge_tmp_tech = pd.merge(video_log[columns], tech_tmp  ,how = 'left', on = 'ID')
 
 a = merge_tmp_tech[['Id','tech name']].drop_duplicates()
