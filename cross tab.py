9510dad6994d05385b9fff5e0c14ecbbb93ca3d7
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-18 15:54:36 +0900

    Update cross tab.py

--- cross tab.py
+++ cross tab.py
@@ -31,3 +31,36 @@ Female	  2	0	2
 # このコードでは、'Gender' カラム（性別）と 'Purchased' カラム（購買履歴）のデータをクロス集計しています。pd.crosstab() 関数により、性別（'Gender'）を行に、購買履歴（'Purchased'）を列に持つクロス集計表が作成されます。
 # この表には、各セルに対応するクロス集計の値が表示されます。例えば、Male（男性）のユーザーのうち、1人が購入しており、Female（女性）のユーザーのうち、1人が購入していることがわかります。
 # pd.crosstab() 関数では、さまざまなオプションが利用でき、集計関数（aggfunc）、合計行と列の表示（margins）、さらなる統計情報の計算などを行うことができます。
+
+# pd.crosstab（クロスタブ）は、Pandasライブラリで提供されているクロス集計（Cross-Tabulation）を行うための便利なメソッドです。
+# クロス集計は、2つ以上のカテゴリカル変数（カテゴリやカテゴリの組み合わせ）に基づいてデータを要約し、クロス集計テーブル（クロスタブ）として表示します。以下に、Pythonコードでpd.crosstabを具体的な例と共に説明します。
+# 例として、あるウェブサイトのユーザーの性別と購買商品カテゴリに関するデータを考えてみましょう。データフレームには、ユーザーごとの性別と購買カテゴリが含まれています。
+
+import pandas as pd
+
+# サンプルのデータを作成
+data = {
+    'User': ['User1', 'User2', 'User3', 'User4', 'User5'],
+    'Gender': ['Male', 'Female', 'Male', 'Female', 'Male'],
+    'Category': ['Electronics', 'Clothing', 'Electronics', 'Electronics', 'Clothing']
+}
+
+df = pd.DataFrame(data)
+
+# このデータを使用して、pd.crosstabを使って性別と購買カテゴリのクロス集計を行います。
+# クロス集計を実行
+cross_tab = pd.crosstab(df['Gender'], df['Category'])
+
+# 上記のコードでは、以下のことが行われています：
+# pd.crosstab メソッドを呼び出し、次のパラメータを指定しています：
+# df['Gender']: クロス集計の行になるカテゴリカル変数を指定します。ここでは性別 ('Gender') 列を指定しています。
+# df['Category']: クロス集計の列になるカテゴリカル変数を指定します。ここでは購買カテゴリ ('Category') 列を指定しています。
+# このコードを実行すると、性別と購買カテゴリに関するクロス集計テーブルが生成されます。出力は次のようになります：
+
+# Category  Clothing  Electronics
+# Gender                         
+# Female           1            1
+# Male             1            2
+
+# このテーブルから、性別と購買カテゴリの組み合わせごとのユーザー数がわかります。
+# たとえば、Female（女性）で Clothing（衣類）を購買したユーザーは1人、Male（男性）で Electronics（電子製品）を購買したユーザーは2人です。pd.crosstabを使用することで、カテゴリカルデータの関係を簡単に可視化および要約できます。
