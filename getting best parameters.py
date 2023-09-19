ba02bd12894f2babf33beef8fffab78b948e7401
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-19 20:42:22 +0900

    Update getting best parameters.py

--- getting best parameters.py
+++ getting best parameters.py
@@ -1,7 +1,7 @@
 # ベストパラメータを呼び出す
 import json
 
-now = filename.replace('./data/F2_dataset_add_features_model_', '').replace('.pkl', '')
+now = filename.replace('xxx', '').replace('.pkl', '')
 path = f"./data/best_params_{now}.json"
 
 with open(path, 'r') as f:
@@ -11,7 +11,7 @@ with open(path, 'r') as f:
 print(d)
 
 # このPythonコードは、JSONファイルから保存されたベストパラメータを読み込む方法を示しています。以下は、コードの詳細な説明です。
-# now = filename.replace('./data/F2_dataset_add_features_model_', '').replace('.pkl', '')：filenameから日付部分を抽出して、対応するベストパラメータのJSONファイルのパスを生成します。このパスは、前のセクションでベストパラメータを保存したファイルのパスと一致する必要があります。
+# now = filename.replace('xxx', '').replace('.pkl', '')：filenameから日付部分を抽出して、対応するベストパラメータのJSONファイルのパスを生成します。このパスは、前のセクションでベストパラメータを保存したファイルのパスと一致する必要があります。
 # with open(path, 'r') as f:：生成されたJSONファイルのパスを使用して、ファイルを読み込みモードで開きます。
 # d_json = f.read()：ファイルからJSONデータを読み込みます。
 # d = json.loads(d_json)：json.loads()関数を使用してJSON文字列をPythonの辞書オブジェクトに変換します。これにより、ベストパラメータがPythonの変数dに読み込まれます。
