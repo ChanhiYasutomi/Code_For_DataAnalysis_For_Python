fd7f35cd10f79335a19b30b73fe2877619aa6028
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-23 13:27:33 +0900

    Update from_dict.py

--- from_dict.py
+++ from_dict.py
@@ -17,9 +17,40 @@ df = pd.DataFrame.from_dict(data)
 print(df)
 
 # このコードでは、data という辞書データを用意し、それを .from_dict() メソッドを使用してデータフレーム df に変換しています。df を表示すると、以下のようなデータフレームが得られます：
-      Name  Age           City
-0    Alice   25       New York
-1      Bob   30  San Francisco
-2  Charlie   35    Los Angeles
+#       Name  Age           City
+# 0    Alice   25       New York
+# 1      Bob   30  San Francisco
+# 2  Charlie   35    Los Angeles
 
 # このように、.from_dict() メソッドは辞書データからデータフレームを生成し、辞書のキーが列名として、各キーに対応する値が列データとしてデータフレームに配置されます。
+
+
+
+# pd.DataFrame.from_dict() メソッドは、Pythonの辞書からPandasのデータフレームを生成するためのメソッドです。具体的に、辞書のキーと値を使用してデータフレームを構築します。提供されたコード例に基づいて、このメソッドの使い方を説明します：
+import pandas as pd
+
+# サンプルの辞書データ
+vector_map = {
+    'A': [1, 0, 0],
+    'B': [0, 1, 0],
+    'C': [0, 0, 1]
+}
+
+# データフレームを生成
+df = pd.DataFrame.from_dict(vector_map, orient='index', columns=[f'Category_{i+1}' for i in range(3)])
+
+# データフレームを表示
+print(df)
+
+# このコードでは、vector_map という辞書データを用意し、それを pd.DataFrame.from_dict() メソッドを使用してデータフレーム df に変換しています。
+# orient='index' パラメータは、辞書のキーを行（インデックス）として使用することを指定しています。つまり、各行のインデックスは 'A'、'B'、'C' に対応します。
+# columns=[f'Category_{i+1}' for i in range(3)] パラメータは、生成されるデータフレームの列名を指定しています。ここでは、3つの列を 'Category_1'、'Category_2'、'Category_3' と命名しています。
+
+# 上記のコードを実行すると、次のようなデータフレーム df が得られます：
+
+#    Category_1  Category_2  Category_3
+# A           1           0           0
+# B           0           1           0
+# C           0           0           1
+
+# このデータフレームでは、各カテゴリ ('A'、'B'、'C') がベクトルとして表現され、各列は 'Category_1'、'Category_2'、'Category_3' という列名で表されています。
