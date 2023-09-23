be100196852dd7c4a10d33c1519afeb4b68efd00
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-23 12:55:04 +0900

    Update plt.bar.py

--- plt.bar.py
+++ plt.bar.py
@@ -22,3 +22,47 @@ plt.title('Histogram of Hour')
 plt.show()
 
 # このコードでは、'hour' 列の値のカウントを取得し、それをMatplotlibを使ってバーグラフ（ヒストグラム）としてプロットしています。必要に応じて、ラベルやタイトルをカスタマイズできます。このコードを実行すると、'hour' 列の値の分布が視覚化されます。
+
+
+
+# plt.bar を使用して棒グラフを描画するコードの具体例を説明します。この例では、df データフレームから 'category' 列のカテゴリごとの出現頻度を取得し、最も出現頻度が高い15個のカテゴリに対して棒グラフを描画します。
+# 以下は具体例です：
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# サンプルのDataFrameを作成
+data = {'category': np.random.choice(['A', 'B', 'C', 'D', 'E'], size=1000)}
+df = pd.DataFrame(data)
+
+# 'category' 列のカテゴリごとの出現頻度を取得
+category_counts = df['category'].value_counts()[:15]
+
+# 棒グラフを描画
+plt.bar(x=category_counts.index,  # カテゴリのラベル
+        height=category_counts.values,  # カテゴリの出現頻度
+        width=0.7)  # 棒の幅
+
+# グラフのタイトルを設定
+plt.title('Top 15 Categories')
+
+# x軸のラベルを回転
+plt.xticks(rotation=45)
+
+# グラフを表示
+plt.show()
+
+# このコードの説明：
+# import matplotlib.pyplot as plt で Matplotlib ライブラリをインポートします。
+# サンプルのDataFrame df を作成し、'category' 列にランダムなカテゴリ（A、B、C、D、Eのいずれか）を持つデータを生成します。
+# df['category'].value_counts()[:15] を使用して、'category' 列のカテゴリごとの出現頻度を計算し、最も出現頻度が高い15個のカテゴリを取得します。
+
+# plt.bar を使用して棒グラフを描画します。以下のパラメータが使用されています：
+# x=category_counts.index: 棒グラフのx軸（カテゴリのラベル）を指定します。
+# height=category_counts.values: 棒グラフの高さ（カテゴリの出現頻度）を指定します。
+# width=0.7: 棒の幅を指定します。
+# plt.title('Top 15 Categories') でグラフにタイトルを設定します。
+# plt.xticks(rotation=45) を使用してx軸のラベルを45度回転させます。これにより、ラベルが重ならずに表示されます。
+# 最後に plt.show() を使用してグラフを表示します。
+
+# これにより、最も出現頻度が高い15個のカテゴリに対する棒グラフが描画され、各カテゴリの出現頻度が可視化されます。
