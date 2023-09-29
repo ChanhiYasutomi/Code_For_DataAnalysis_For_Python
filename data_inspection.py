77c2a85b69dd2b8551b931ebf0e5b1a3c44d2397
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-30 01:46:43 +0900

    Update data_inspection.py

--- data_inspection.py
+++ data_inspection.py
@@ -63,6 +63,40 @@ data_inspection = pd.DataFrame({
 
 display(data_inspection)
 
+# 以下はdefでまとめたもの
+def inspect_dataframe(df):
+    data_inspection = pd.DataFrame({
+        'Column Name': df.columns,
+        'Data Type': df.dtypes.values,
+        'Number of rows': len(df),
+        'Number of uniques': df.nunique(),
+        'Non-Null Count': df.count().values,
+        'Null Count': df.isnull().sum(),
+        'Null rate': (df.isnull().sum() / len(df)).round(5),
+        'date min': df['年月日'].min(),
+        'date max': df['年月日'].max(),
+
+        'mean': df.mean(numeric_only=True),
+        'median': df.median(numeric_only=True),
+        'mode': df.mode(numeric_only=True).iloc[0],
+        'mode rate': (df.mode(numeric_only=True).iloc[0] / len(df)),
+        'outliers rate': df.apply(find_outliers).round(5),
+        'std': df.std(numeric_only=True),
+        'min': df.min(numeric_only=True),
+        '25 percentile': df.quantile(0.25, numeric_only=True),
+        '50 percentile': df.quantile(0.50, numeric_only=True),
+        '75 percentile': df.quantile(0.75, numeric_only=True),
+        'max': df.max(numeric_only=True),
+
+        'flag or not': df.apply(unique_to_binary),
+        'columns details': None,
+        'remarks': None,
+        'trigger': None,
+        'data example': df.head(1).T[0]
+    }).reset_index(drop=True)
+
+    return data_inspection
+    
 # 提供されたコードに基づいて、データセットの検査および情報収集のためのフレームワークが設定されています。これにより、データセットの品質を評価し、データの特性に関する洞察を得るのに役立つ情報が収集されます。以下は提供されたコードの詳細な説明です：
 # unique_to_binary 関数: この関数は、各カラムに対してユニークな値が0または1であるかどうかを確認し、その結果をフラグとして返します。これにより、カラムごとにバイナリフラグを設定することができます。たとえば、特定のカラムに2つのユニークな値（0と1）しか含まれない場合、そのカラムに対するフラグが1に設定されます。
 # コメントアウトされた outlier_percentage 関数: この関数は、各数値カラムに対して外れ値の割合を計算するために使用できるものです。外れ値は、平均から5標準偏差以上離れた値として定義されます。この関数を復活させてカスタマイズすることで、外れ値の評価が可能になります。
