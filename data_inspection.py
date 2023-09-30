23a55e7ac3266e26bef5c6acc1f396cbf73c0b1d
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-30 11:23:56 +0900

    Update data_inspection.py

--- data_inspection.py
+++ data_inspection.py
@@ -11,7 +11,30 @@ def unique_to_binary(column):
     else:
         return 0
 
+# def find_outliers(column):
+#     # データの平均値と標準偏差を計算
+#     mean = column.mean()
+#     std = column.std()
+    
+#     # 外れ値の閾値を設定
+#     threshold = 2  # この値を調整して外れ値の基準を変更
+    
+#     # 外れ値を検出する条件を設定
+#     lower_bound = mean - threshold * std
+#     upper_bound = mean + threshold * std
+    
+#     # 外れ値を検出
+#     outliers = (column < lower_bound) | (column > upper_bound)
+
+#     # 外れ値の割合を算出
+#     outliers_rate = outliers.sum() / len(column)
+#     return outliers_rate #,outliers
+
 def find_outliers(column):
+    # 数値型の列でない場合、エラーメッセージを表示して処理を中止
+    if not pd.api.types.is_numeric_dtype(column):
+        raise ValueError("列は数値型である必要があります。")
+
     # データの平均値と標準偏差を計算
     mean = column.mean()
     std = column.std()
@@ -28,7 +51,8 @@ def find_outliers(column):
 
     # 外れ値の割合を算出
     outliers_rate = outliers.sum() / len(column)
-    return outliers_rate #,outliers
+    return outliers, outliers_rate
+    # return outliers_rate #, outliers # こうしたら割合を算出できる。
 
 # データ検品
 data_inspection = pd.DataFrame({
