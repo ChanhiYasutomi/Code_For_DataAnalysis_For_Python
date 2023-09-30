e77475acbea67a52181ee51984a56019fed91c9d
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-09-30 11:21:43 +0900

    Update def find_outliers.py

--- def find_outliers.py
+++ def find_outliers.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def find_outliers(column):
+    # 数値型の列でない場合、エラーメッセージを表示して処理を中止
+    if not pd.api.types.is_numeric_dtype(column):
+        raise ValueError("列は数値型である必要があります。")
+
     # データの平均値と標準偏差を計算
     mean = column.mean()
     std = column.std()
@@ -21,11 +25,12 @@ def find_outliers(column):
     # return outliers_rate #, outliers # こうしたら割合を算出できる。
 
 # データセットを作成
-data = {'Value': [10, 15, 20, 25, 30, 100, 105, 110, 115, 120, 600, 600]}
+data = {'A': [1, 2, 3, 4, 5, 1000], 'B': ["a", "a", "a", "a", "a", "a"]}
 df = pd.DataFrame(data)
 
-# find_outliers 関数を適用して 'Value' 列の外れ値と割合を検出
-outliers, percentage = find_outliers(df['Value'])
+# find_outliers 関数を適用して 'A' 列の外れ値と割合を検出
+outliers, percentage = find_outliers(df['A']) #これは正常に処理される
+# outliers, percentage = find_outliers(df['B']) #これは数値方じゃないからエラーが出る(raise ValueError("列は数値型である必要があります。"))
 
 # 外れ値を表示
 display("外れ値:", outliers)
