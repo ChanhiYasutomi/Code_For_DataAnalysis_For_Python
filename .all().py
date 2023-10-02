010bda7791c4e9a39313a147ab0c10cd09f860c6
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-10-02 23:29:21 +0900

    Update .all().py

--- .all().py
+++ .all().py
@@ -29,3 +29,27 @@ False
 
 # この結果からわかるように、各列に対して.all() メソッドを適用した結果、すべての要素がTrueでないため、各列の結果はすべてFalseとなっています。
 # このように、.all() メソッドは、列やシリーズ内の要素がすべてTrueであるかどうかを簡単に確認するために役立ちます。
+
+
+
+import pandas as pd
+
+# サンプルデータを含むデータフレームを作成
+data = {
+    'A': [True, True, False, True],
+    'B': [True, False, False, True],
+    'C': [True, False, False, False]
+}
+
+df = pd.DataFrame(data)
+
+# 各行に対して.all(1)メソッドを適用し、各行内の要素がすべてTrueであるかどうかを確認
+row_1_result = df.iloc[0].all()
+row_2_result = df.iloc[1].all()
+row_3_result = df.iloc[2].all()
+row_4_result = df.iloc[3].all()
+
+True
+False
+False
+False
