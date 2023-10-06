3795cb63ef37abd9cfa18e81c2a9ddefb3d78df5
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-10-07 08:18:34 +0900

    Create pd.to_datetime().py

---
+++ pd.to_datetime().py
@@ -0,0 +1,22 @@
+提供されたコードは、Pandasを使用して日付情報が含まれる列をdatetimeオブジェクトに変換する方法を示しています。
+pd.to_datetime() メソッドは、日付または時刻を含む列をPandasのdatetimeオブジェクトに変換するのに役立ちます。
+以下は具体的な例です：
+import pandas as pd
+
+# サンプルデータを作成
+data = {'time': ['2021-10-14', '2021-10-15', '2021-10-16']}
+df_before_groupby = pd.DataFrame(data)
+
+# 'time' 列をdatetimeオブジェクトに変換
+df_before_groupby['time'] = pd.to_datetime(df_before_groupby['time'])
+
+# 結果を表示
+print(df_before_groupby)
+
+# このコードでは、df_before_groupby データフレームの 'time' 列を pd.to_datetime() メソッドを使用してdatetimeオブジェクトに変換しています。
+# 変換後のデータフレームは、 'time' 列がdatetimeオブジェクトになります：
+#   time
+# 0                        2021-10-14
+# 1                        2021-10-15
+# 2                        2021-10-16
+# これにより、日付情報が含まれる列をdatetimeオブジェクトとして扱うことができ、日付に関するさまざまな操作や解析を行うのに便利です。
