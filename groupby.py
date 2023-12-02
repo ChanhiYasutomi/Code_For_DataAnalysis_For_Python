49d3e6fb074d14bda29712bf7a8eb0d02cce6ef4
Chani Leon <96760611+ChanhiYasutomi@users.noreply.github.com>
2023-12-02 18:14:50 +0900

    Update groupby.py

--- groupby.py
+++ groupby.py
@@ -111,3 +111,52 @@ print(result)
 # 1        B     30
 # 2        C     70
 # このように、as_index=False を指定することで、グループ化結果が新しいDataFrameの列として保持され、インデックスにならないようになります。
+
+
+
+# groupbyメソッドを使用することで、DataFrameを特定の列（ここでは'Gender'列）の値に基づいてグループに分け、その後に各グループに対して統計的な操作を行うことができます。
+# 以下に一般的な例を示し、Pythonの具体的なコードで説明します。
+# 仮想的なデータを用いて例を示します：
+import pandas as pd
+
+# 仮想的なデータを作成
+data = pd.DataFrame({
+    'Gender': ['Male', 'Female', 'Male', 'Female', 'Male', 'Female'],
+    'Age': [25, 30, 22, 35, 28, 32],
+    'Salary': [50000, 60000, 45000, 70000, 55000, 80000]
+})
+
+# 'Gender'列を基準にグループ化
+groupby_gender = data.groupby('Gender')
+
+# 各グループに対して平均、中央値、データ数、合計を計算
+mean_values = groupby_gender.mean()
+median_values = groupby_gender.median()
+count_values = groupby_gender.count()
+sum_values = groupby_gender.sum()
+
+# 結果を表示
+display("Mean Values:")
+display(mean_values)
+
+display("\nMedian Values:")
+display(median_values)
+
+display("\nCount Values:")
+display(count_values)
+
+display("\nSum Values:")
+display(sum_values)
+
+# このコードでは、groupby_genderによって'Gender'列を基準にデータがグループ化されています。
+# その後、各統計量（平均、中央値、データ数、合計）が計算されています。これにより、異なる性別に関連する統計情報が得られます。
+
+# データフレームdataが以下のようになっていると仮定します：
+#   Gender  Age  Salary
+# 0   Male   25   50000
+# 1 Female   30   60000
+# 2   Male   22   45000
+# 3 Female   35   70000
+# 4   Male   28   55000
+# 5 Female   32   80000
+# 上記のコードを実行すると、各性別に対して平均、中央値、データ数、合計が計算され、それぞれの結果が表示されます。
